0b171197b6b6f144824d849d60050ab5c877490a
jiakai <jiakai@gridsum.com>
2016-10-26 11:19:24 +0800

    message interface definition

---
+++ src/PACE.entity/message/IMessage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace PACE.entity.message
+{
+	/// <summary>
+	/// Runtime collected data interfaces
+	/// Author : JiaKai
+	/// </summary>
+	public interface IMessage
+	{
+		/// <summary>
+		/// Initialized to be 'TRUE'.
+		/// </summary>
+		bool IsSuccess { get; set; }
+
+		/// <summary>
+		/// Return whether the Complete() method was called, false means not. 
+		/// </summary>
+		bool IsComplete { get; }
+
+		/// <summary>
+		/// Get the key-value pairs data
+		/// </summary>
+		object Data { get; }
+
+		/// <summary>
+		/// Get the	message name.
+		/// </summary>
+		string Name { get;}
+
+		/// <summary>
+		/// Get the message status, "0" means success, otherwise error code.
+		/// </summary>
+		string Status { get; }
+
+		/// <summary>
+		/// Get the message type.
+		/// </summary>
+		MessageType Type { get; }
+
+		/// <summary>
+		/// Get the time stamp message was created.
+		/// </summary>
+		DateTime Timestamp { get; }
+
+		/// <summary>
+		/// add one or multiple key-value pairs to the message.
+		/// </summary>
+		/// ReSharper disable CSharpWarnings::CS1570
+		/// <param name="keyValuePairs">key-value pairs like 'a=1&b=2&c=3...'</param>
+		/// ReSharper restore CSharpWarnings::CS1570
+		void AddData(string keyValuePairs);
+
+		/// <summary>
+		/// add one key-value pair to the message.
+		/// </summary>
+		/// <param name="key">key-value pair key</param>
+		/// <param name="value">key-value pair value</param>
+		void AddData(string key, object value);
+
+		/// <summary>
+		/// Set the message status.
+		/// </summary>
+		/// <param name="status">"0" means success, otherwise error code</param>
+		void SetStatus(string status);
+
+		/// <summary>
+		/// Set the message status.
+		/// </summary>
+		/// <param name="e">exception and Derived exceptions</param>
+		void SetStatus(Exception e);
+
+		/// <summary>
+		/// Complete the message construction.
+		/// </summary>
+		void Complete();
+	}
+}
---
+++ src/PACE.entity/message/MessageType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PACE.entity.message
+{
+	[Serializable]
+	public enum MessageType
+	{
+		/// <summary>
+		/// maps to one resource url
+		/// </summary>
+		Url,
+		/// <summary>
+		/// maps to one method of service call
+		/// </summary>
+		Service,
+		/// <summary>
+		/// maps to one method of search call
+		/// </summary>
+		Search,
+		/// <summary>
+		/// maps to internal method call
+		/// </summary>
+		Method,
+		/// <summary>
+		/// maps to one SQL statement
+		/// </summary>
+		Sql,
+		/// <summary>
+		/// maps to one cache access
+		/// </summary>
+		Cache,
+		/// <summary>
+		/// maps to Exception 
+		/// </summary>
+		Error
+	}
+}
